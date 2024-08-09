30787a12c125465402273d0c5f92982ab7ca0d69
halayumu <ookamiayumuwww@gmail.com>
2024-08-10 08:57:14 +0900

    #11 新規住所入力画面作成、新規住所入力画面呼び出しコントローラ作成

---
+++ resources/views/newAddresView.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>新規住所</title>
+</head>
+<body>
+
+  <section id="new_address">
+    <h2>手動で住所を入力してください！</h2>
+    <input type="text">
+    <button>登録</button>
+  </section>
+
+</body>
+</html>
